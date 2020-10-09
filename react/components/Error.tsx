ef83678c684c24e4ac38c49e30d7158c8cd3f22e
victorhmp <victor2142@gmail.com>
2020-10-09 15:31:07 -0300

    Remove unnecessary wrapper div elements

--- react/components/Error.tsx
+++ react/components/Error.tsx
@@ -2,17 +2,15 @@ import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { useCssHandles } from 'vtex.css-handles'
 
-const CSS_HANDLES = ['defaultErrorContainer', 'defaultErrorMessage'] as const
+const CSS_HANDLES = ['defaultErrorMessage'] as const
 
 function DefaultError() {
   const handles = useCssHandles(CSS_HANDLES)
 
   return (
-    <div className={`flex justify-center pv5 ${handles.defaultErrorContainer}`}>
-      <p className={handles.defaultErrorMessage}>
-        <FormattedMessage id="store/newsletter-submit-error.default" />
-      </p>
-    </div>
+    <p className={handles.defaultErrorMessage}>
+      <FormattedMessage id="store/newsletter-submit-error.default" />
+    </p>
   )
 }
 
--- react/components/Success.tsx
+++ react/components/Success.tsx
@@ -2,22 +2,15 @@ import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { useCssHandles } from 'vtex.css-handles'
 
-const CSS_HANDLES = [
-  'defaultSuccessContainer',
-  'defaultSuccessMessage',
-] as const
+const CSS_HANDLES = ['defaultSuccessMessage'] as const
 
 function DefaultSuccess() {
   const handles = useCssHandles(CSS_HANDLES)
 
   return (
-    <div
-      className={`flex justify-center pv5 ${handles.defaultSuccessContainer}`}
-    >
-      <p className={handles.defaultSuccessMessage}>
-        <FormattedMessage id="store/newsletter-submit-success.default" />
-      </p>
-    </div>
+    <p className={handles.defaultSuccessMessage}>
+      <FormattedMessage id="store/newsletter-submit-success.default" />
+    </p>
   )
 }
 
