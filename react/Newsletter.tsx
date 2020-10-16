a0b9a59c1e8fd5a609631c9df59aafa276beb7fd
victorhmp <victor2142@gmail.com>
2020-10-16 10:34:23 -0300

    Add regex101 link for testing the email validation regex

--- react/Newsletter.tsx
+++ react/Newsletter.tsx
@@ -18,7 +18,9 @@ interface Props {
 
 const CSS_HANDLES = ['newsletterForm'] as const
 
-const EMAIL_REGEX = /^[A-z0-9+_-]+(?:\.[A-z0-9+_-]+)*@(?:[A-z0-9](?:[A-z0-9-]*[A-z0-9])?\.)+[A-z0-9](?:[A-z0-9-]*[A-z0-9])?$/
+// https://regex101.com/r/926sxf/1
+// If you change the regex, don't forget to go to the link above, update it there and save.
+const EMAIL_REGEX = /^[A-z0-9"+_-]+(?:\.[A-z0-9+_-]+)*@(?:[A-z0-9](?:[A-z0-9-]*[A-z0-9])?\.)+[A-z0-9](?:[A-z0-9-]*[A-z0-9])?$/
 
 function validateEmail(email: string) {
   return EMAIL_REGEX.test(email)
