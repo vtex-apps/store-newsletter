d6edea6e91c6ce98995530fab0384155040ee4ad
victorhmp <victor2142@gmail.com>
2020-10-20 18:54:20 -0300

    Create FormPhoneInput component and 'newsletter-input-phone' interface

---
+++ react/FormPhoneInput.tsx
@@ -0,0 +1,66 @@
+import React, { ChangeEvent, useEffect } from 'react'
+import { useIntl } from 'react-intl'
+import { Input } from 'vtex.styleguide'
+import { IOMessage, formatIOMessage } from 'vtex.native-types'
+import { useCssHandles } from 'vtex.css-handles'
+
+import {
+  useNewsletterDispatch,
+  useNewsletterState,
+} from './components/NewsletterContext'
+
+interface Props {
+  placeholderText?: string
+  inputLabel?: string
+  errorMessage?: string
+}
+
+const CSS_HANDLES = ['phoneInputContainer', 'phoneInputLabel'] as const
+
+function FormPhoneInput(props: Props) {
+  const {
+    placeholderText = 'store/newsletter-input-phone.placeholderText.default',
+    errorMessage = 'store/newsletter-input-phone.errorMessage.default',
+    inputLabel,
+  } = props
+
+  const { invalidPhone } = useNewsletterState()
+  const dispatch = useNewsletterDispatch()
+  const handles = useCssHandles(CSS_HANDLES)
+  const intl = useIntl()
+
+  // Initialize `phone` context value to signal that there is a FormPhoneInput
+  // being rendered inside the newsletter form.
+  useEffect(() => dispatch({ type: 'UPDATE_PHONE', value: '' }), [dispatch])
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: 'UPDATE_PHONE', value: e.target.value.trim() })
+  }
+
+  return (
+    <div className={handles.phoneInputContainer}>
+      <label
+        className={handles.phoneInputLabel}
+        htmlFor="newsletter-input-phone"
+      >
+        <IOMessage id={inputLabel} />
+      </label>
+      <Input
+        id="newsletter-input-phone"
+        name="newsletter"
+        type="tel"
+        onChange={handleChange}
+        errorMessage={
+          invalidPhone ? formatIOMessage({ id: errorMessage, intl }) : null
+        }
+        placeholder={formatIOMessage({ id: placeholderText, intl })}
+      />
+    </div>
+  )
+}
+
+FormPhoneInput.schema = {
+  title: 'admin/editor.newsletter-input-phone.title',
+}
+
+export default FormPhoneInput
