f0971aaeff8bf842b96563d4224a6478377c4dda
victorhmp <victor2142@gmail.com>
2020-12-18 12:09:04 -0300

    Add useRuntime mock definition

--- react/__mocks__/vtex.render-runtime.ts
+++ react/__mocks__/vtex.render-runtime.ts
@@ -1,1 +1,8 @@
 export const useChildBlock = () => true
+
+export const useRuntime = () => ({
+  binding: {
+    id: 'aacb06b3-a8fa-4bab-b5bd-2d654d20dcd8',
+    canonicalBaseAddress: 'storetheme.vtex.com/',
+  },
+})
